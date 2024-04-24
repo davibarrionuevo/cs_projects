515e60043f821a5a34858d8659265147d0d21918
davibarrionuevo <davibarrionuevo01@gmail.com>
2024-04-24 13:50:50 -0300

    Comments added to the Calculator and to the TodoList.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,9 +2,11 @@
 Console.WriteLine("Input the first number:");
 string firstInput = Console.ReadLine();
 int number1 = int.Parse(firstInput);
+// parses the first input into an integer
 Console.WriteLine("Input the second number:");
 string secondInput = Console.ReadLine();
 int number2 = int.Parse(secondInput);
+// parses the second input into an integer
 Console.WriteLine("What do you want to do with those numbers?");
 Console.WriteLine("[A]dd");
 Console.WriteLine("[S]ubtract");
@@ -39,11 +41,15 @@ else
 Console.WriteLine("Press any key to close");
 Console.ReadKey();
 
+// this method gathers the operation components and prints it into an equation
 void PrintFinalEquation(int number1, int number2, int result, string @operator)
 {
     Console.WriteLine(number1 + " " + @operator + " " + number2 + " = " + result);
 }
 
+// this method checks if the operationChoice input given by the user is equal to a certain letter,
+// whether upper or lowercase, that will select the operation to be done:
+// A or a for addition, S or s for subtraction, M or m for multiplication, D or d for division
 bool EqualsCaseInsensitive(string left, string right)
 {
     return left.ToUpper() == right.ToUpper();
--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -3,6 +3,8 @@
 Console.WriteLine("Welcome to your TODO list manager!");
 
 bool shallExit = false;
+// this variable will only be true if the user selects the '[E]xit' option;
+// while false, the program will function in a loop
 
 while (!shallExit)
 {
@@ -37,6 +39,8 @@ while (!shallExit)
     }
 }
 
+// if the list is not empty, this method shows all the TODOs that have been added,
+// with the elements starting with index 1
 void SeeAllTodos()
 {
     if (todoList.Count == 0)
@@ -53,6 +57,9 @@ void SeeAllTodos()
     }
 }
 
+// this method keeps asking the user to provide a TODO description until
+// it meets the requirements set in the IsDescriptionValid method; if the
+// description is valid, it will be added into the list
 void AddTodo()
 {
     string userDescription;
@@ -67,6 +74,9 @@ void AddTodo()
 
 }
 
+// this method checks if the TODO description given by the user is not
+// empty and if it hasn't given before, that is, it is a unique one;
+// if these requirements are met, the description is set as valid
 bool IsDescriptionValid(string userDescription)
 {
     if (userDescription == "")
@@ -82,6 +92,9 @@ bool IsDescriptionValid(string userDescription)
     return true;
 }
 
+// this method checks if the list is not empty; if not, it shows its elements
+// and keeps asking the user to provide the index of the TODO that will be removed
+// until the TryReadIndex method returns true
 void RemoveTodo()
 {
     if (todoList.Count == 0)
@@ -99,14 +112,11 @@ void RemoveTodo()
     RemoveTodoAtIndex(index - 1);
 }
 
-void RemoveTodoAtIndex(int index)
-{
-    var todoToBeRemoved = todoList[index];
-    todoList.RemoveAt(index);
-    Console.WriteLine("TODO removed: " + todoToBeRemoved);
-
-}
-
+// this method tries to read the given index to remove a certain TODO;
+// if the input is empty, it will return false; if it can be parsed as
+// an integer, it will return true if the index is larger or equal to 1 
+// and if it is smaller or equal to the count of elements from the list;
+// if it cannot be parsed as an integer, it will also return false
 bool TryReadIndex(out int index)
 {
     var selectedIndex = Console.ReadLine();
@@ -126,6 +136,19 @@ bool TryReadIndex(out int index)
     return false;
 }
 
+// this method removes the TODO with the given index and shows
+// its description
+void RemoveTodoAtIndex(int index)
+{
+    var todoToBeRemoved = todoList[index];
+    todoList.RemoveAt(index);
+    Console.WriteLine("TODO removed: " + todoToBeRemoved);
+
+}
+
+
+
+// this method simply prints a message that there are no TODOs in the list
 static void ShowNoTodosMessage()
 {
     Console.WriteLine("No TODOs have been added yet.");
